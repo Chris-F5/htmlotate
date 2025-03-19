9320f0d1b0aae9fdbeaa0df72b472e6d44859b1c
Christopher Lang <christopher.lang.256@gmail.com>
2025-03-19 11:01:02 +0000

    Fix htmlotate-refs

--- htmlotate-refs.py
+++ htmlotate-refs.py
@@ -5,6 +5,13 @@ import re
 
 for line in sys.stdin:
     line = line.strip('\n').strip('\r')
-    line = re.sub(r'[^^]\[(.+)\]', r'<a href="#ref-\1">[\1]</a>', line)
-    line = re.sub(r'^\[(.+)\]', r'<a id="ref-\1">[\1]</a>', line)
+    if match := re.search(r'^(\s*)\[([^\]]+)\](.*)$', line):
+        leading_whitespace = match.group(1)
+        ref = match.group(2)
+        trailing_text = match.group(3)
+        line = leading_whitespace \
+             + f'<a id="#ref-{ref}">[{ref}]</a>' \
+             + re.sub(r'\[([^\]]+)\]', r'<a href="#ref-\1">[\1]</a>', trailing_text)
+    else:
+        line = re.sub(r'\[([^\]]+)\]', r'<a href="#ref-\1">[\1]</a>', line)
     print(line)
